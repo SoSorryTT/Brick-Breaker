6f673e0f3a8616b5bbdbaac4ae1c54b7b5471112
Panu Tanavatavivat <markpanya@gmail.com>
2023-05-06 21:55:20 +0700

    change message layout

--- src/OptionPaneLost.java
+++ src/OptionPaneLost.java
@@ -3,7 +3,7 @@ public class OptionPaneLost {
     JFrame lost;  
     OptionPaneLost() {  
         lost = new JFrame();  
-        int a = JOptionPane.showConfirmDialog(lost, "Try again","You lost!!!", JOptionPane.YES_NO_OPTION);
+        int a = JOptionPane.showConfirmDialog(lost, "Try again?","You lost!!!", JOptionPane.YES_NO_OPTION);
         if (a == JOptionPane.YES_OPTION) {  
             GUI gui = new GUI();
             gui.start();
